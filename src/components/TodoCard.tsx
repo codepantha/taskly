252a428abcb539a328f29adc2939ba3575186022
codepanther <pezechigozirim@gmail.com>
2022-05-09 23:41:52 +0100

    edit task items

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Todo } from '../model';
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
@@ -11,6 +11,18 @@ type Props = {
 };
 
 const TodoCard: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
+  const [editedTask, setEditedTask] = useState<string>(todo.name);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTodos(
+      todos.map((t) => {
+        if (t.id === todo.id) todo.name = editedTask;
+        return t;
+      })
+    );
+  }, [editedTask])
+
   const handleDone = (id: number) => {
     setTodos(
       todos.map((todo) =>
@@ -19,18 +31,43 @@ const TodoCard: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
     );
   };
 
+  const handleEdit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsEditing(false);
+  };
+
+  const handleChange = (e: React.FormEvent) => {
+    const target = e.target as HTMLInputElement;
+    setEditedTask(target.value);
+  }
+
   const handleDelete = (id: number) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   return (
-    <form className="todos__single">
-      <span className={`todos__single--text ${todo.isCompleted ? 'dash' : ''}`}>
-        {todo.name}
-      </span>
+    <form className="todos__single" onSubmit={handleEdit}>
+      {isEditing ? (
+        <input
+          onChange={handleChange}
+          className={`todos__single--text ${todo.isCompleted ? 'dash' : ''}`}
+          value={editedTask}
+          autoFocus
+        />
+      ) : (
+        <span
+          className={`todos__single--text ${todo.isCompleted ? 'dash' : ''}`}
+        >
+          {todo.name}
+        </span>
+      )}
 
       <div>
-        <button type="button" className="icon">
+        <button
+          type="button"
+          className="icon"
+          onClick={() => setIsEditing(!isEditing)}
+        >
           <AiFillEdit />
         </button>
         <button
