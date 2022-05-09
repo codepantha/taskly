0da2d6ffabb55bfea7f86f79c12acf1c9363cb20
codepanther <pezechigozirim@gmail.com>
2022-05-09 20:20:24 +0100

    implement 'done' and delete todo functionalities

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Todo } from '../model'
-import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
+import React from 'react';
+import { Todo } from '../model';
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
 import './styles.css';
 
@@ -8,26 +8,48 @@ type Props = {
   todo: Todo;
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-}
+};
 
 const TodoCard: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <form className="todos__single">
-      <span className="todos__single--text">{todo.name}</span>
+      <span className={`todos__single--text ${todo.isCompleted ? 'dash' : ''}`}>
+        {todo.name}
+      </span>
 
       <div>
         <button type="button" className="icon">
           <AiFillEdit />
         </button>
-        <button type="button" className="icon">
+        <button
+          type="button"
+          className="icon"
+          onClick={() => handleDelete(todo.id)}
+        >
           <AiFillDelete />
         </button>
-        <button type="button" className="icon">
+        <button
+          type="button"
+          className="icon"
+          onClick={() => handleDone(todo.id)}
+        >
           <MdDone />
         </button>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default TodoCard
+export default TodoCard;
