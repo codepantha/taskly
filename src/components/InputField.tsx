0ba940407445a1d7a53bfa950c31df19e138673b
codepanther <pezechigozirim@gmail.com>
2022-05-09 13:46:23 +0100

    remove focus from input using useRef and blur

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './styles.css';
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
-  handleAdd: (e: React.FormEvent) => void
+  handleAdd: (e: React.FormEvent) => void;
 }
 
 const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input" onSubmit={handleAdd}>
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
+        ref={inputRef}
         type="text"
         name="task"
         placeholder="Enter a task"
