77c2eb4ddbecc3d597f71f2b36c030b4633dc845
codepanther <pezechigozirim@gmail.com>
2022-05-10 01:43:42 +0100

    change app layout to have active and completed tasks

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -10,10 +10,19 @@ interface Props {
 
 const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
   return (
-    <div className="todos">
-      { todos.map(todo => (
-        <TodoCard key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />
-      ))}
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">Active Tasks</span>
+        { todos.map(todo => (
+          <TodoCard key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />
+        ))}
+      </div>
+      <div className="todos remove">
+        <span className="todos__heading">Completed Tasks</span>
+        { todos.map(todo => (
+          <TodoCard key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />
+        ))}
+      </div>
     </div>
   )
 }
